cc80de2658e7cc89fee9afb42a883d76c174997d
Toorreess <jostorpos@uma.es>
2025-06-10 23:11:22 +0200

    ✨ feat: Order model updated

--- domain/model/order.go
+++ domain/model/order.go
@@ -6,6 +6,7 @@ type Order struct {
 	Amount           float64           `json:"amount" firestore:"amount" validate:"required" updateAllowed:"true"`
 	SelectedProducts []SelectedProduct `json:"products" firestore:"products" validate:"required"`
 	Status           string            `json:"status" firestore:"status" validate:"required,oneof=pending paid cancelled" updateAllowed:"true"`
+	MemberID         string            `json:"member_id" firestore:"member_id" validate:"required"`
 
 	// Deleted is used for logical deletion
 	Deleted bool `json:"-" firestore:"deleted"`
@@ -15,5 +16,4 @@ type SelectedProduct struct {
 	ID       string  `json:"id" firestore:"id" validate:"required"`
 	Quantity int     `json:"quantity" firestore:"quantity" validate:"required"`
 	Price    float64 `json:"price" firestore:"price" validate:"required"`
-	Tax      string  `json:"tax" firestore:"tax"`
 }
