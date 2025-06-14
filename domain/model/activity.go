e0cb7fb2b370ca9a0fd5524d3f08e841d2521a5f
Toorreess <jostorpos@uma.es>
2025-06-14 20:05:29 +0200

    🔧 change: allow updates to activity status

--- domain/model/activity.go
+++ domain/model/activity.go
@@ -5,7 +5,7 @@ type Activity struct {
 	Name        string `json:"name" firestore:"name" validate:"required" updateAllowed:"true"`
 	Duration    int    `json:"duration" firestore:"duration" updateAllowed:"true"`
 	MaxCapacity int    `json:"max_capacity" firestore:"max_capacity"`
-	IsActive    bool   `json:"is_active" firestore:"is_active"`
+	IsActive    bool   `json:"is_active" firestore:"is_active" updateAllowed:"true"`
 
 	// Deleted is used for logical deletion
 	Deleted bool `json:"-" firestore:"deleted"`
