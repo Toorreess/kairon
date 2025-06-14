cd1c3b41fe28423fb4d008a5d04ddbeff8d3cfb0
Toorreess <jostorpos@uma.es>
2025-06-14 16:38:05 +0200

    🐛 fix: typo in model tag

--- domain/model/member.go
+++ domain/model/member.go
@@ -3,7 +3,7 @@ package model
 type Member struct {
 	ID           string `json:"id" firestore:"-"`
 	Name         string `json:"name" firestore:"name" validate:"required" updateAllowed:"true"`
-	Identifier   string `json:"identifier" firesotre:"identifier" validate:"required" updateAllowed:"true"`
+	Identifier   string `json:"identifier" firestore:"identifier" validate:"required" updateAllowed:"true"`
 	Email        string `json:"email" firestore:"email" validate:"required,email" updateAllowed:"true"`
 	Phone        string `json:"phone" firestore:"phone" updateAllowed:"true"`
 	Status       string `json:"status" firestore:"status" validate:"oneof=active inactive" updateAllowed:"true"`
